7cad6aa8bc9249b1951e8dcaf24073e30cecb73e
MohamedMankouchi <mohamed.mankouchi@student.ehb.be>
2023-01-19 14:50:23 +0100

    added current position

--- Front-end/home.js
+++ Front-end/home.js
@@ -1,8 +1,21 @@
 console.log('OK')
 
-// initialize Leaflet
-var map = L.map('map').setView([51.505, -0.09], 10);
+var map = L.map('map').setView([50.85045, 4.34878], 10);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+
+let location = navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+    enableHighAccuracy: true
+})
+
+function successLocation(position) {
+    L.marker([position.coords.latitude, position.coords.longitude]).addTo(map)
+}
+
+function errorLocation() {
+    L.marker([position.coords.latitude, position.coords.longitude]).addTo(map)
+
+}
