0d2953dda1d3ace9eb1b9b11b279cf02680db1c4
DinaBouazzafen <91288579+DinaBouazzafen@users.noreply.github.com>
2023-01-24 10:20:46 +0100

    Pop-up console button aangepast

--- Front-end/pop-up.js
+++ Front-end/pop-up.js
@@ -5,20 +5,15 @@ const explenation = document.querySelector(".explenation");
 const information = document.querySelector(".pop-up-home");
 const doorgaanConsole = document.querySelector("#doorgaanConsole");
 
-if (eerstebezoek == "no") {
+if (eerstebezoek == "Not firstVisit") {
  removeConsole();
 }
 
-
-
 doorgaanConsole.addEventListener("click", removeConsole);
-
-
 
 function removeConsole() {
     explenation.style.display = "none";
     information.style.display = "none";
 
 }
-
-localStorage.setItem("firstVisit", "no");
+localStorage.setItem("firstVisit", "Not firstVisit");
