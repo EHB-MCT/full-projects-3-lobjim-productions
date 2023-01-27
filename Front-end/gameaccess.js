d08bdad8141e37754cd14f28bffe284e28485a50
DinaBouazzafen <91288579+DinaBouazzafen@users.noreply.github.com>
2023-01-27 08:55:32 +0100

    CHnages in yuku's spel unlock system, error in going through another page

--- Front-end/gameaccess.js
+++ Front-end/gameaccess.js
@@ -1,8 +1,6 @@
 //get from hidden inputfield html
-let code = "GEVANGENIS";
-let gameId = 'spel1'
-
-//const password = document.querySelector("#password").value;
+let code = document.querySelector("#pswrd-spel").value;
+let gameId = document.querySelector("#gameId").value;
 
 const form = document.querySelector("form");
 form.addEventListener("submit", function(event) {
@@ -11,9 +9,8 @@ form.addEventListener("submit", function(event) {
     if (password === code) {
         //update password given naar 'GAMEID'
         localStorage.setItem(gameId, "true")
-        //document.getElementById('locked_1').style.display = "none";
+        window.location.href = './../installatie-games/'+gameId+'.html';
 
-        window.location.href = './../installatie-games/cat-game.html';
 
     } else {
         document.querySelector('#error-message').innerHTML = 'Incorrect password. Please try again.';
@@ -22,27 +19,5 @@ form.addEventListener("submit", function(event) {
 
 //update password given naar 'GAMEID'
 if (localStorage.getItem(gameId)) {
-    //document.getElementById('locked_1').style.display = "none";
-    window.location.href = './../installatie-games/cat-game.html';
+    window.location.href = './../installatie-games/'+gameId+'.html';
 }
-
-
-// let gameId;
-
-// const form = document.querySelector("form");
-// form.addEventListener("submit", function(event) {
-//     event.preventDefault();
-//     const password = document.querySelector("#password").value;
-//     gameId = document.querySelector("#game-id").value;
-//     const code = document.querySelector("#pswrd-"+gameId).value;
-//     if (password === code) {
-//         localStorage.setItem(gameId, "true");
-//         window.location.href = './../installatie-games/'+gameId+'.html';
-//     } else {
-//         document.querySelector('#error-message').innerHTML = 'Incorrect password. Please try again.';
-//     }
-// });
-
-// if (localStorage.getItem(gameId)) {
-//     window.location.href = './../installatie-games/'+gameId+'.html';
-// }
