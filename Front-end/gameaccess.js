ef732ead80eb5488231a21d84a089ea185c9c163
DinaBouazzafen <91288579+DinaBouazzafen@users.noreply.github.com>
2023-01-27 09:04:29 +0100

    Using localstorage to put the code and gameId

--- Front-end/gameaccess.js
+++ Front-end/gameaccess.js
@@ -1,23 +1,14 @@
-//get from hidden inputfield html
-let code = document.querySelector("#pswrd-spel").value;
-let gameId = document.querySelector("#gameId").value;
-
 const form = document.querySelector("form");
 form.addEventListener("submit", function(event) {
     event.preventDefault();
     const password = document.querySelector("#password").value;
+    const gameId = localStorage.getItem("gameId");
+    const code = localStorage.getItem("code");
     if (password === code) {
-        //update password given naar 'GAMEID'
         localStorage.setItem(gameId, "true")
         window.location.href = './../installatie-games/'+gameId+'.html';
-
 
     } else {
         document.querySelector('#error-message').innerHTML = 'Incorrect password. Please try again.';
     }
 });
-
-//update password given naar 'GAMEID'
-if (localStorage.getItem(gameId)) {
-    window.location.href = './../installatie-games/'+gameId+'.html';
-}
