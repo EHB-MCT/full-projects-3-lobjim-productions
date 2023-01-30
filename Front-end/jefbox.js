e5dcb8996e0713735f3ef6214bc999022743efdc
DinaBouazzafen <91288579+DinaBouazzafen@users.noreply.github.com>
2023-01-30 10:46:46 +0100

    game 1 unlock at all times

--- Front-end/jefbox.js
+++ Front-end/jefbox.js
@@ -1,9 +1,13 @@
 const gameIds = ['cat-game', 'vrije-yuku', 'not-yet', 'not-yet', 'not-yet', 'not-yet'];
 
 gameIds.forEach((gameId, i) => {
+  if (i === 0) {
+    localStorage.setItem(gameId, 'true');
+  }
   if (localStorage.getItem(gameId) === 'true') {
     document.getElementById(`locked_${i+1}`).style.display = 'none';
   }
 });
+
 
 
