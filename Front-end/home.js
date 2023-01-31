401a9baa3863da602fa05427d749993b6e371608
QuentinMignon <quentin.mignon@student.ehb.be>
2023-01-31 10:49:39 +0100

    new pop up for login

--- Front-end/home.js
+++ Front-end/home.js
@@ -1295,11 +1295,17 @@ function renderToiletData(findToilet) {
                 })
         } else {
             Swal.fire({
-                position: 'top-end',
-                icon: 'info',
-                title: 'Log je in om te liken!',
-                showConfirmButton: false,
-                timer: 2000
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire('Deleted!', 'Your file has been deleted.', 'success')
+                }
             })
         }
 
