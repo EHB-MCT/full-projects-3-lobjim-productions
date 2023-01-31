b53930a2c73de0ec20e4903eac24baeb41de2d4f
DinaBouazzafen <91288579+DinaBouazzafen@users.noreply.github.com>
2023-01-31 21:43:30 +0100

    Tracking a score for every game played

---
+++ Front-end/track-score.js
@@ -0,0 +1,15 @@
+const gameIds = ['cat-game', 'vrije-yuku', 'not-yet', 'not-yet', 'not-yet', 'not-yet'];
+
+let score = parseInt(localStorage.getItem('score'), 10) || 0;
+
+gameIds.forEach((gameId, i) => {
+  if (localStorage.getItem(gameId) === 'true') {
+    score += 100;
+  }
+});
+
+localStorage.setItem('score', score);
+
+document.querySelector('#score').innerHTML = score;
+
+
