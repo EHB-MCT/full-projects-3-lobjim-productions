a73b04d9e92e96157accc5cba9ee3eb58daa2451
DinaBouazzafen <91288579+DinaBouazzafen@users.noreply.github.com>
2023-02-01 19:14:57 +0100

    Bug fixed for cat-game points

--- installatie-games/cat-game.js
+++ installatie-games/cat-game.js
@@ -1,4 +1,4 @@
-import {updateScoreForPlayedGame} from './../Front-end/track-score.js';
+import {updateScoreForPlayedGame, getScore} from './../Front-end/track-score.js';
 
 document.getElementById('replay').style.display = "none";
 const animationURL = document.getElementById('gif-cat');
@@ -22,18 +22,20 @@ function rotateImage() {
         rotation = (rotation + angle) % 360;
         rotated.style.transform = `rotate(${rotation}deg)`;
         rotationCounter++;
-
+        // Start game
+        localStorage.setItem('isPlayingCatGame', true);
 
     } else {
-        // If the rotation counter is 600 or more, change the image source to the animation URL
+        // If the rotation counter is 600 or more, change image source to the animation URL
         rotated.style.transform = `rotate(0deg)`;
         rotated.src = animationURL.src;
         animationURL.style.display = "block";
         // Remove the touchmove event listener to stop the rotation
         document.getElementById("rotate").removeEventListener('touchmove', rotateImage);
         document.getElementById('replay').style.display = "block";
-        // Update score
-        updateScoreForPlayedGame();
+        // End game
+        localStorage.removeItem('isPlayingCatGame');
+
     }
 }
 
@@ -41,3 +43,12 @@ document.getElementById("rotate").addEventListener('touchmove', event => {
     rotateImage();
     console.log(event)
 });
+
+// Update  score based on status of cat-game
+if (!localStorage.getItem('isPlayingCatGame')) {
+    let score = updateScoreForPlayedGame();
+    // Add 20 points to  score
+    score += 20;
+    getScore()
+  }
+
